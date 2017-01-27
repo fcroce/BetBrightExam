60c4495ed63fd7155269715c0142f91fad60f024
fcroce <crocefabiopa@gmail.com>
2017-01-27 17:20:24 +0100

    Part 2 in progress

--- assets/js/jstest.js
+++ assets/js/jstest.js
@@ -22,7 +22,7 @@ var flickrClass = function(search_output, results_output) {
     };
 
     this.loadXMLDoc = function() {
-        var flickrURL = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=' + search;
+        var flickrURL = 'https://www.flickr.com/services/feeds/photos_public.gne?format=json&tags=' + search;
 
         // Ajax CORS request
         function createCORSRequest(method, url) {
